a1931af3881131850394169f0a8cb35c76368ec9
ezenjun <ezenjun07@gmail.com>
2024-10-07 10:30:14 +0900

    github pages 설정

--- 3. Frontend/ReactVite/src/main.tsx
+++ 3. Frontend/ReactVite/src/main.tsx
@@ -6,11 +6,11 @@ import { CounterProvider } from './hooks/counter-hook';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <CounterProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CounterProvider>
-  </StrictMode>
+	<StrictMode>
+		<CounterProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</CounterProvider>
+	</StrictMode>
 );
